4c25ac410dacafbaf30fa08742914fe95cd64f2f
dev-divine7 <devdivine8@gmail.com>
2025-05-16 10:52:45 +0100

    Fixed all emails

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,50 +1,55 @@
 const allMenu = [
   {
-  image: "assets/images/food-menu/jollofrice-and-fried-chicken.jpg",
-  nameOfFood: "Jollof Rice and Fried Chicken",
-  description: "Delicious Nigerian Jollof rice served with crispy fried chicken and rich tomato stew.",
-  price: "#20,000",
-  whatsappLink: `https://wa.me/2348169383907?text=*Taste%20Haven%20Restaurant%20Services*%0A%0AHello%20please%20I%20would%20like%20to%20order%20Jollof%20Rice%20and%20Fried%20Chicken.`,
-  email: `mailto:bludbrro@gmail.com?subject=Taste%20Haven%20Order&body=*Taste%20Haven%20Restaurant%20Services*%0A%0AHello%20please%20I%20would%20like%20to%20order%20Jollof%20Rice%20and%20Fried%20Chicken.`
+    image: 'assets/images/food-menu/jollofrice-and-fried-chicken.jpg',
+    nameOfFood: 'Jollof Rice and Fried Chicken',
+    description:
+      'Delicious Nigerian Jollof rice served with crispy fried chicken and rich tomato stew.',
+    price: '#20,000',
+    whatsappLink: `https://wa.me/2348169383907?text=*Taste%20Haven%20Restaurant%20Services*%0A%0AHello%20please%20I%20would%20like%20to%20order%20Jollof%20Rice%20and%20Fried%20Chicken.`,
+    email: `mailto:devdivine18@gmail.com?subject=Taste%20Haven%20Order&body=*Taste%20Haven%20Restaurant%20Services*%0A%0AHello%20please%20I%20would%20like%20to%20order%20Jollof%20Rice%20and%20Fried%20Chicken.`,
   },
   {
-    image: "assets/images/food-menu/ofada-rice-and-ayamase.jpg",
-    nameOfFood: "Ofada Rice and Ayamase",
-    description: "Local Nigerian ofada rice served with spicy ayamase (designer stew).",
-    price: "#20,000",
+    image: 'assets/images/food-menu/ofada-rice-and-ayamase.jpg',
+    nameOfFood: 'Ofada Rice and Ayamase',
+    description:
+      'Local Nigerian ofada rice served with spicy ayamase (designer stew).',
+    price: '#20,000',
     whatsappLink: `https://wa.me/2348169383907?text=*Taste%20Haven%20Restaurant%20Services*%0A%0AHello%20please%20I%20would%20like%20to%20order%20Ofada%20Rice%20and%20Ayamase.`,
-    email: `mailto:bludbrro@gmail.com?subject=Taste%20Haven%20Order&body=*Taste%20Haven%20Restaurant%20Services*%0A%0AHello%20please%20I%20would%20like%20to%20order%20Ofada%20Rice%20and%20Ayamase.`
+    email: `mailto:devdivine18@gmail.com?subject=Taste%20Haven%20Order&body=*Taste%20Haven%20Restaurant%20Services*%0A%0AHello%20please%20I%20would%20like%20to%20order%20Ofada%20Rice%20and%20Ayamase.`,
   },
   {
-    image: "assets/images/food-menu/yam-and-goat-meat-pepper-soup.jpg",
-    nameOfFood: "Yam and Goat Meat Pepper Soup",
-    description: "Soft-boiled yam served with spicy goat meat pepper soup.",
-    price: "#20,000",
+    image: 'assets/images/food-menu/yam-and-goat-meat-pepper-soup.jpg',
+    nameOfFood: 'Yam and Goat Meat Pepper Soup',
+    description: 'Soft-boiled yam served with spicy goat meat pepper soup.',
+    price: '#20,000',
     whatsappLink: `https://wa.me/2348169383907?text=*Taste%20Haven%20Restaurant%20Services*%0A%0AHello%20please%20I%20would%20like%20to%20order%20Yam%20and%20Goat%20Meat%20Pepper%20Soup.`,
-    email: `mailto:bludbrro@gmail.com?subject=Taste%20Haven%20Order&body=*Taste%20Haven%20Restaurant%20Services*%0A%0AHello%20please%20I%20would%20like%20to%20order%20Yam%20and%20Goat%20Meat%20Pepper%20Soup.`
+    email: `mailto:devdivine18@gmail.com?subject=Taste%20Haven%20Order&body=*Taste%20Haven%20Restaurant%20Services*%0A%0AHello%20please%20I%20would%20like%20to%20order%20Yam%20and%20Goat%20Meat%20Pepper%20Soup.`,
   },
   {
-    image: "assets/images/food-menu/amala-and-ewedu-soup.jpg",
-    nameOfFood: "Amala and Ewedu Soup",
-    description: "Delicious amala served with ewedu soup, gbegiri, and assorted meat.",
-    price: "#20,000",
+    image: 'assets/images/food-menu/amala-and-ewedu-soup.jpg',
+    nameOfFood: 'Amala and Ewedu Soup',
+    description:
+      'Delicious amala served with ewedu soup, gbegiri, and assorted meat.',
+    price: '#20,000',
     whatsappLink: `https://wa.me/2348169383907?text=*Taste%20Haven%20Restaurant%20Services*%0A%0AHello%20please%20I%20would%20like%20to%20order%20Amala%20and%20Ewedu%20Soup.`,
-    email: `mailto:bludbrro@gmail.com?subject=Taste%20Haven%20Order&body=*Taste%20Haven%20Restaurant%20Services*%0A%0AHello%20please%20I%20would%20like%20to%20order%20Amala%20and%20Ewedu%20Soup.`
+    email: `mailto:devdivine18@gmail.com?subject=Taste%20Haven%20Order&body=*Taste%20Haven%20Restaurant%20Services*%0A%0AHello%20please%20I%20would%20like%20to%20order%20Amala%20and%20Ewedu%20Soup.`,
   },
   {
-    image: "assets/images/food-menu/fried-rice-and-grilled-chicken.jpg",
-    nameOfFood: "Fried Rice and Grilled Chicken",
-    description: "Well-seasoned fried rice served with tasty grilled chicken and sauce.",
-    price: "#20,000",
+    image: 'assets/images/food-menu/fried-rice-and-grilled-chicken.jpg',
+    nameOfFood: 'Fried Rice and Grilled Chicken',
+    description:
+      'Well-seasoned fried rice served with tasty grilled chicken and sauce.',
+    price: '#20,000',
     whatsappLink: `https://wa.me/2348169383907?text=*Taste%20Haven%20Restaurant%20Services*%0A%0AHello%20please%20I%20would%20like%20to%20order%20Fried%20Rice%20and%20Grilled%20Chicken.`,
-    email: `mailto:bludbrro@gmail.com?subject=Taste%20Haven%20Order&body=*Taste%20Haven%20Restaurant%20Services*%0A%0AHello%20please%20I%20would%20like%20to%20order%20Fried%20Rice%20and%20Grilled%20Chicken.`
+    email: `mailto:devdivine18@gmail.com?subject=Taste%20Haven%20Order&body=*Taste%20Haven%20Restaurant%20Services*%0A%0AHello%20please%20I%20would%20like%20to%20order%20Fried%20Rice%20and%20Grilled%20Chicken.`,
   },
   {
-    image: "assets/images/food-menu/spicy-suya-beef.jpg",
-    nameOfFood: "Spicy Suya Beef",
-    description: "Well-marinated beef suya, served with onions, tomatoes, and spicy suya pepper.",
-    price: "#20,000",
+    image: 'assets/images/food-menu/spicy-suya-beef.jpg',
+    nameOfFood: 'Spicy Suya Beef',
+    description:
+      'Well-marinated beef suya, served with onions, tomatoes, and spicy suya pepper.',
+    price: '#20,000',
     whatsappLink: `https://wa.me/2348169383907?text=*Taste%20Haven%20Restaurant%20Services*%0A%0AHello%20please%20I%20would%20like%20to%20order%20Spicy%20Suya%20Beef.`,
-    email: `mailto:bludbrro@gmail.com?subject=Taste%20Haven%20Order&body=*Taste%20Haven%20Restaurant%20Services*%0A%0AHello%20please%20I%20would%20like%20to%20order%20Spicy%20Suya%20Beef.`
+    email: `mailto:devdivine18@gmail.com?subject=Taste%20Haven%20Order&body=*Taste%20Haven%20Restaurant%20Services*%0A%0AHello%20please%20I%20would%20like%20to%20order%20Spicy%20Suya%20Beef.`,
   },
 ];
